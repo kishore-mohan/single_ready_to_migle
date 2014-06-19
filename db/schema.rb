8928823da9e5d38d17c2e7cdc086528ff4ae4e25
Kishore.mohan <kishoremohan@SMNDT-314.(none)>
2014-06-19 12:26:22 +0530

    added password column into user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140618170241) do
+ActiveRecord::Schema.define(:version => 20140619065309) do
 
   create_table "projects", :force => true do |t|
     t.string   "name"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(:version => 20140618170241) do
     t.integer  "project_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "password"
   end
 
 end
