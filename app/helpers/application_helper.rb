db950de206c56d75139992b376a7cac820dd735b
Kishore.mohan <kishoremohan@SMNDT-314.(none)>
2014-06-19 19:23:09 +0530

    UI/UX changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def flash_class(level)
+    case level
+      when :notice then "alert fade in alert-info"
+      when :success then "alert fade in alert-success"
+      when :error then "alert fade in alert-error"
+      when :alert then "alert fade in alert-error"
+    end
+end
 end
