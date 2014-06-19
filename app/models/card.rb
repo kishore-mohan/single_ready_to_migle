4d045f4dc9092c018affeaee8c194d17e11ed429
Bammidi Rajasekhar <bammidi.rajasekhar@gmail.com>
2014-06-20 03:47:07 +0530

    card shwoing

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,6 +2,7 @@ class Card < ActiveRecord::Base
   attr_accessible :estimate,:comments
   belongs_to :user
   belongs_to :card
+  belongs_to :project
   validates_uniqueness_of :number
   has_many :estimation_details
 end
