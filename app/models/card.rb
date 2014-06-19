f7cb5cc89077efb95f4d07838be0810fbba705e1
Kishore.mohan <kishoremohan@SMNDT-314.(none)>
2014-06-20 03:42:14 +0530

    UI/UX chnages

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,6 +2,6 @@ class Card < ActiveRecord::Base
   attr_accessible :estimate,:comments
   belongs_to :user
   belongs_to :card
-  validates_uniqueness_of :number
+  validates_uniqueness_of :number, :scope => :project_id
   has_many :estimation_details
 end
